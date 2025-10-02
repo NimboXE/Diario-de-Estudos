73468e9979008efae11bed59d7f2ed143f1aab4b
Frogguzzz <ranchow3@gmail.com>
2025-10-02 10:36:12 -0300

    Media & Statics Configuration Commit

--- setup/settings.py
+++ setup/settings.py
@@ -10,7 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+## Importing OS & DotEnv ##
 from pathlib import Path
+import dotenv
+import os
+
+## Loading DotEnv ##
+dotenv.load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o6=b(r#a8o4_sxv%^#s@(4rluc8xq=-n_#tsa4iz#e#mcav8dj'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -118,6 +124,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+## Configurando os arquivos staticos ##
+STATICFILES_DIR = [os.path.join(BASE_DIR, 'app/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+## Configurando os arquivos de mídia ##
+MEDIA_DIR = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- setup/urls.py
+++ setup/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+## Importando os caminhos para a mídia ##
+from django.conf.urls.static import static
+from django.conf import settings
+
+## No fim do colchete, será implementado o caminho para os arquivos de mídia ##
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
