2d3560ff5db08f8829ecd1ca802c0ba5204459cb
Frogguzzz <ranchow3@gmail.com>
2025-10-08 07:24:48 -0300

    First Migration Build & Auth User Model Commit

--- setup/settings.py
+++ setup/settings.py
@@ -137,3 +137,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+## Integração do AUTH USER para o usuário criado ##
+AUTH_USER_MODEL = 'app.Usuario'
